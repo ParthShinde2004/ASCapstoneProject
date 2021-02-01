b5a6a132501a8618badc178e70aebb5702a286c6
ParthShinde2004 <204789@gardenschool.edu.my>
2021-02-01 21:33:06 +0800

    Made some small changes and added notes to explain the code

--- Quiz.py
+++ Quiz.py
@@ -2,17 +2,19 @@
 & Queues (plus other topics) and add all the user's score to a separate file."""
 
 
-# Class for questions that contain question attributes
+# Class for questions so that the code is much more efficient and smaller
 class Question:
     def __init__(self, prompt, answer):
         self.prompt = prompt
         self.answer = answer
 
 
+# An array with all questions
 question_prompts = [
-    "1", "2", "3"
+    "Question 1", "Question 2", "Question 3"
 ]
 
+# Used the class to make a array with the questions and answers
 questions = [
     Question(question_prompts[0], "a"),
     Question(question_prompts[1], "b"),
@@ -25,7 +27,7 @@ def run_quiz(questions):
     name = str(input("What is your name?"))
     highscorefile = open("highscore.txt", "a")
     score = 0
-    for question in questions:
+    for question in questions: # Loops through all the questions
         answer = input(question.prompt)
         if answer == question.answer:
             score += 1
